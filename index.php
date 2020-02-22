6b57ef44cefade9f810fce876b038ad97325432d
Healer404 <47404070+healer404@users.noreply.github.com>
2020-02-22 16:28:08 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,292 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DAPO | Data Analytics on Pathological Outbreak</title>
+
+    <!-- css files sections -->
+    <link rel="shortcut icon" href="./assets/custom/img/DAPOAppLogo.png" type="text/css">
+    <link rel="stylesheet" href="./assets/custom/css/index.css" type="text/css">
+    <link rel="stylesheet" href="./assets/lib/bootstrap/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="./assets/lib/fontawesome/css/all.min.css" type="text/css">
+    <!-- END css files -->
+</head>
+<body>
+    <!-- Navbar Section -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-snow" id="HomeSection">
+        <div class="container">
+            <a class="navbar-brand p-0 m-0" href="#">
+                <img src="./assets/custom/img/DAPOLogo.png" alt="DAPO App Logo and Text" width="200" height="60">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#dapoNavbarContents" aria-controls="dapoNavbarContents" aria-expanded="false" aria-label="DAPO Navbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="dapoNavbarContents">
+                <ul class="navbar-nav ml-auto font-weight-bold ">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#HomeSection">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#DownloadSection">Download</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">DAPO Map</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">DAPO Team</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- END Navbar section -->
+
+    <!-- Home Section -->
+    <div class="home-dapo bg-dapo">
+        <div class="home-dapo-screener p-0 m-0">
+            <div class="row pt-3">
+                <div class="col-md-7">
+                    <div class="row dapo-content">
+                        <div class="col-md-12 pl-4">
+                            <img src="./assets/custom/img/dapo-text.png" alt="DAPO text" height="150" width="300">
+                        </div>
+                        <div class="col-md-12 text-left text-snow pl-5">
+                            <div class="dapo-text-header pl-2">
+                                Data Analytics on Pathological Outbreaks
+                            </div>
+                            <div class="dapo-text-subheader pl-2">
+                                Join to fight these pathological outbreaks in the Philippines
+                            </div>
+                        </div>
+                        <div class="col-md-12 pt-4">
+                            <div class="row">
+                                <div class="col-md-12"></div>
+                                <div class="col-md-7 text-justify pl-5">
+                                    <a href="#KnowMoreSection" class="btn-know-more">
+                                        KNOW MORE
+                                    </a>
+                                    <a href="#DownloadSection" class="btn-dapo">
+                                        JOIN NOW
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-5"></div>
+            </div>
+            <div class="row pt-2">
+                <div class="col-md-12">&nbsp;</div>
+                <div class="col-md-9"></div>
+                <div class="col-md-2 credit-to py-1 pl-4">
+                    <a href="https://www.freepik.com/free-photos-vectors/medical" class="text-white">Medical photo created by jcomp - www.freepik.com</a>
+                </div>
+                <div class="col-md-1"></div>
+            </div>
+        </div>
+    </div>
+    <!-- END Home Section -->
+
+    <!-- Download Section Divider -->
+    <div class="container-fluid bg-dapo-divider">
+        <div class="row p-2"></div>
+    </div>
+    <!-- END Download Section Divider -->
+
+    <!-- Know more Section -->
+    <div class="container pt-5 mb-5" id="KnowMoreSection">
+        <div class="card card-know-more">
+            <div class="row">
+                <div class="col-md-2">
+                    <div class="card-body text-center text-dapo">
+                        <i class="fas fa-question-circle fa-6x"></i>
+                    </div>
+                </div>
+                <div class="col-md-10">
+                    <div class="card-body">
+                        Here to present to you the application designed for people to volunteer on crowdsourcing data of mosquito whereabouts and to be able record its biting activities for future researches.<br><br>
+                        DAPO - Data Analytics on Pathological Outbreaks is a community spearheaded by VLCT, Inc.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END Know more Section -->
+    
+    <!-- Download Section Divider -->
+    <div class="container-fluid bg-dapo-divider">
+        <div class="row p-2"></div>
+    </div>
+    <!-- END Download Section Divider -->
+
+    <!-- Download Section -->
+    <div class="container-fluid" id="DownloadSection">
+        <div class="row">
+            <div class="col-md-12 pt-3">
+                <div class="download-text-header text-center">
+                    Join the DAPO Community
+                </div>
+            </div>
+            <div class="col-md-1"></div>
+            <div class="col-md-10 download-contaier">
+                <div class="row">
+                    <div class="col-md-1"></div>
+                    <div class="col-md-3 pt-5 pb-5 download-carousel">
+                        <div id="dapoCarouselIndicators" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-target="#dapoCarouselIndicators" data-slide-to="0" class="active"></li>
+                                <li data-target="#dapoCarouselIndicators" data-slide-to="1"></li>
+                                <li data-target="#dapoCarouselIndicators" data-slide-to="2"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="./assets/custom/img/monitor-dapo.png" class="d-block w-100" alt="pic 1">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="./assets/custom/img/monitor-dapo.png" class="d-block w-100" alt="pic 2">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="./assets/custom/img/monitor-dapo.png" class="d-block w-100" alt="pic 3">
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#dapoCarouselIndicators" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#dapoCarouselIndicators" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-1"></div>
+                    <div class="col-md-6">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="download-header text-center pt-5">
+                                    How to join the DAPO community?
+                                </div>
+                            </div>
+                            <div class="col-md-12">&nbsp;</div>
+                            <div class="col-md-12">&nbsp;</div>
+                            <div class="col-md-2 text-center text-snow pt-3">
+                                <i class="fas fa-file-download fa-3x"></i>
+                            </div>
+                            <div class="col-md-10 text-snow download-content">
+                                Download the DAPO app package in this site.
+                            </div>
+                            <div class="col-md-12">&nbsp;</div>
+                            <div class="col-md-2 text-center text-snow pt-3">
+                                <i class="fas fa-download fa-3x"></i>
+                            </div>
+                            <div class="col-md-10 text-snow download-content">
+                                Install the DAPO app package in to your device.
+                            </div>
+                            <div class="col-md-12">&nbsp;</div>
+                            <div class="col-md-2 text-center text-snow pt-3">
+                                <i class="fas fa-universal-access fa-3x"></i>
+                            </div>
+                            <div class="col-md-10 text-snow download-content">
+                                Use the DAPO app to help in fighting these pathological outbreaks
+                            </div>
+                            <div class="col-md-12">&nbsp;</div>
+                            <div class="col-md-12">&nbsp;</div>
+                            <div class="col-md-12 text-center">
+                                <a href="#" class="download-btn">
+                                    <div class="btn-group" role="group" aria-label="Android Download">
+                                        <button type="button" class="btn btn-light">
+                                            <img src="./assets/custom/img/android-logo.png" alt="Android Logo" height="50" width="50">
+                                        </button>
+                                        <button type="button" class="btn btn-light">
+                                            <span class="download-btn">
+                                                Download
+                                            </span>
+                                        </button>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-md-12 text-center text-snow pb-5">
+                                Download now and become part of a data-driven community.
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END Download Section -->
+
+    <!-- Download Section Divider -->
+    <div class="container-fluid bg-dapo-divider">
+        <div class="row p-2"></div>
+    </div>
+    <!-- END Download Section Divider -->
+
+    <!-- DAPO Sub Footer -->
+    <div class="container-fluid pt-5 bg-green">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-5">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <img src="./assets/custom/img/dapo-text.png" alt="DAPO Text" height="90" width="200">
+                                </div>
+                                <div class="col-md-12 p-2 dapo-footer-links">
+                                    <a href="#">Home</a>
+                                </div>
+                                <div class="col-md-12 p-2 dapo-footer-links">
+                                    <a href="#">Terms and Conditions</a>
+                                </div>
+                                <div class="col-md-12 p-2 dapo-footer-links">
+                                    <a href="#">Privacy Policy</a>
+                                </div>
+                                <div class="col-md-12 p-2 pb-5 dapo-footer-links">
+                                    <a href="#">About</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="footer-support-header text-snow">
+                                Supported by
+                            </div>
+                            <div class="footer-support-content pt-2 pb-5">
+                                <img src="./assets/custom/img/bns_logo.jpg" alt="BNS Logo" height="100" width="200">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="footer-contact-header text-snow">
+                                Contact Us
+                            </div>
+                            <div class="foooter-contact-content text-snow pt-2 pb-5">
+                                <i class="fas fa-address-card"></i> dapoteam@dapo.com <br>
+                                <i class="fas fa-phone"></i> 0990-909-0900 <br>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END DAPO Sub Footer -->
+
+    <!-- DAPO Footer -->
+    <div class="container-fluid p-3 bg-lightgreen text-center">
+        Copyright &copy; 2020 Project DAPO Data Analytics on Pathological Outbreaks. All Right Reserved
+    </div>
+    <!-- END DAPO Footer -->
+
+    <!-- js files sections -->
+    <script src="./../assets/lib/bootstrap/js/jquery.slim.min.js"></script>
+    <script src="./../assets/lib/bootstrap/js/popper.js"></script>
+    <script src="./../assets/lib/bootstrap/js/bootstrap.min.js"></script>
+    <!-- END js files sections -->
+</body>
+</html>
